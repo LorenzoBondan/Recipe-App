00142fd8ce70a5dc2e4b80674f30000ad4dde341
LorenzoBondan <lbbondan@gmail.com>
2025-04-08 08:44:29 -0300

    add Recipe List page

---
+++ lib/view/recipe/recipe_list_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/services/recipe_service.dart';
+import 'package:provider/provider.dart';
+
+class RecipeListPage extends StatelessWidget {
+  const RecipeListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final service = Provider.of<RecipeService>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Recipes')),
+      body: service.isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: service.findAll.length,
+              itemBuilder: (context, index) {
+                final item = service.findAll[index];
+                return ListTile(
+                  title: Text(item.name),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+
+                      IconButton(
+                        icon: const Icon(Icons.visibility, color: Colors.green),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RecipeDetailsPage(recipe: item),
+                            ),
+                          );
+                        },
+                      ),
+
+                      IconButton(
+                        icon: const Icon(Icons.edit),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RecipeFormPage(recipe: item),
+                            ),
+                          );
+                        },
+                      ),
+                      
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: () async {
+                          await service.delete(item.id);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Recipe deleted')),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const RecipeFormPage(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
