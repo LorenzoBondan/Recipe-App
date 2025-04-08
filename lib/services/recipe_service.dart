878df4468581de381965cf813aedfbe7ec667bc4
LorenzoBondan <lbbondan@gmail.com>
2025-04-08 08:41:23 -0300

    add recipeService

---
+++ lib/services/recipe_service.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/entities/Recipe.dart';
+import 'package:recipe_app/repositories/Recipe_repository.dart';
+
+class RecipeService extends ChangeNotifier {
+  final RecipeRepository repository = RecipeRepository();
+
+  List<Recipe> _recipes = [];
+  bool _isLoading = true;
+
+  List<Recipe> get findAll => _recipes;
+  bool get isLoading => _isLoading;
+
+  RecipeService() {
+    Future.microtask(() => loadRecipes());
+  }
+
+  Future<void> loadRecipes() async {
+    _isLoading = true;
+    notifyListeners();
+
+    _recipes = await repository.findAll();
+
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  Future<Recipe?> findById(int id) async {
+    return await repository.findById(id);
+  }
+
+  Future<void> create(Recipe obj) async {
+    await repository.create(obj);
+    await loadRecipes();
+  }
+
+  Future<void> update(Recipe obj) async {
+    await repository.update(obj);
+    await loadRecipes();
+  }
+
+  Future<void> delete(int id) async {
+    await repository.delete(id);
+    await loadRecipes();
+  }
+}
