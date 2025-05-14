12b28605e9521fb164944fe5b790faa9fbde0f3d
Sthefany Lopes <sthefany.lopes.cc@gmail.com>
2025-05-14 10:26:41 -0300

    update RecipeListPage styles

--- lib/view/recipe/recipe_list_page.dart
+++ lib/view/recipe/recipe_list_page.dart
@@ -3,6 +3,7 @@ import 'package:recipe_app/services/recipe_service.dart';
 import 'package:recipe_app/view/recipe/recipe_details_page.dart';
 import 'package:recipe_app/view/recipe/recipe_form_page.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 class RecipeListPage extends StatefulWidget {
   const RecipeListPage({super.key});
@@ -20,19 +21,39 @@ class _RecipeListPageState extends State<RecipeListPage> {
       context: context,
       builder: (BuildContext dialogContext) {
         return AlertDialog(
-          title: const Text("Confirm Deletion"),
-          content: const Text("Are you sure you want to delete this Recipe?"),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          title: const Text(
+            'Confirm Deletion',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          content: const Text(
+            'Are you sure you want to delete this Recipe?',
+            textAlign: TextAlign.center,
+          ),
+          actionsAlignment: MainAxisAlignment.center,
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(dialogContext),
-              child: const Text("Cancel"),
+              child: Text(
+                'Cancel',
+                style: TextStyle(
+                  color: Colors.grey[800] ?? const Color.fromARGB(255, 66, 66, 66),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ),
-            TextButton(
+            ElevatedButton(
               onPressed: () {
                 Provider.of<RecipeService>(context, listen: false).delete(recipeId);
                 Navigator.pop(dialogContext);
               },
-              child: const Text("Delete", style: TextStyle(color: Colors.red)),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+              ),
+              child: const Text('Delete'),
             ),
           ],
         );
@@ -49,26 +70,29 @@ class _RecipeListPageState extends State<RecipeListPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Recipes', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 255, 255))),
-        backgroundColor: Colors.deepPurple,
+        title: const Text('Recipes', style: TextStyle(color: Color.fromARGB(255, 255, 255, 255), fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
         elevation: 5,
       ),
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
-
+            padding: const EdgeInsets.only(top: 16.0, right: 8.0, bottom: 8.0, left: 8.0),
             child: TextField(
               controller: _searchController,
               decoration: InputDecoration(
-                labelText: 'Search Recipe',
-                labelStyle: TextStyle(color: Colors.deepPurple),
-                prefixIcon: const Icon(Icons.search, color: Colors.deepPurple),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.deepPurple)),
-                enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.deepPurple)),
-                hintText: 'Search...',
-                hintStyle: TextStyle(color: Colors.deepPurple.withValues(alpha: 0.6)),
+                prefixIcon: Icon(Icons.search, color: Theme.of(context).primaryColorDark),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey[700] ?? const Color.fromARGB(255, 97, 97, 97)),
+                  borderRadius: BorderRadius.circular(3.0)
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey[500] ?? const Color.fromARGB(255, 158, 158, 158)),
+                  borderRadius: BorderRadius.circular(3.0)
+                ),
+                hintText: 'Search Recipe',
+                hintStyle: TextStyle(color: Colors.grey[700]),
               ),
               onChanged: (value) {
                 setState(() {
@@ -76,49 +100,89 @@ class _RecipeListPageState extends State<RecipeListPage> {
                 });
               },
             ),
-
           ),
+
           Expanded(
             child: ListView.builder(
               itemCount: filteredRecipes.length,
               itemBuilder: (context, index) {
                 final item = filteredRecipes[index];
-                return ListTile(
-                  title: Text(item.name),
-                  subtitle: Text('Preparation time: ${item.preparationTimeMinutes} minutes | Rate: ${item.rate}'),
-
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-
-                      IconButton(
-                        icon: const Icon(Icons.visibility, color: Colors.green),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => RecipeDetailsPage(recipe: item),
+                return Card(
+                  margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.all(16.0),
+                    title: Text(
+                      item.name,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: TextStyle(color: Colors.grey[900], fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          DateFormat('MM/dd/yyyy').format(item.addedDate),
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                        SizedBox(height: 2),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Icons.access_time, color: Theme.of(context).primaryColorDark, size: 16),
+                            SizedBox(width: 4),
+                            Text(
+                              '${item.preparationTimeMinutes} ${item.preparationTimeMinutes == 1 ? "minute" : "minutes"}',
+                              style: TextStyle(color: Colors.grey[900]),
                             ),
-                          );
-                        },
-                      ),
-
-                      IconButton(
-                        icon: const Icon(Icons.edit),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => RecipeFormPage(recipe: item)),
-                          );
-                        },
-                      ),
-
-                      IconButton(
-                        icon: const Icon(Icons.delete, color: Colors.red),
-                        onPressed: () => _confirmDelete(context, item.id!),
-                      ),
-
-                    ],
+                          ],
+                        ),
+                        SizedBox(height: 2),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Icons.star, color: Colors.amber[700], size: 16),
+                            SizedBox(width: 4),
+                            Text(
+                              item.rate.toString(),
+                              style: TextStyle(color: Colors.grey[900]),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.visibility, color: Colors.green[600]),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => RecipeDetailsPage(recipe: item),
+                              ),
+                            );
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.edit),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => RecipeFormPage(recipe: item)),
+                            );
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete, color: Colors.red),
+                          onPressed: () => _confirmDelete(context, item.id!),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
@@ -127,13 +191,17 @@ class _RecipeListPageState extends State<RecipeListPage> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => const RecipeFormPage()),
           );
         },
+        backgroundColor: Theme.of(context).primaryColor,
+        foregroundColor: Colors.white,
+        elevation: 5,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: const Icon(Icons.add),
       ),
     );
   }
