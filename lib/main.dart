1f3e75eec55125366e97044b179e9a95a9855dd7
LorenzoBondan <lbbondan@gmail.com>
2025-06-07 11:04:47 -0300

    add and configure firebase-auth

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,16 @@ import 'package:recipe_app/services/random_service.dart';
 import 'package:recipe_app/services/recipe_ingredient_service.dart';
 import 'package:recipe_app/services/recipe_service.dart';
 import 'package:recipe_app/view/recipe/recipe_list_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   await DatabaseHelper().database;
 
   runApp(const MyApp());
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,80 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<String?> login({required String email, required String senha}) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: senha);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "user-not-found":
+          return "Email inválido";
+        case "wrong-password":
+          return "Senha inválida";
+      }
+      return e.code;
+    }
+
+    return null;
+  }
+
+  Future<String?> register({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      UserCredential userCredential =
+          await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      await userCredential.user!.updateDisplayName(name);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "email-already-in-use":
+          return "Email already in use.";
+      }
+      return e.code;
+    }
+
+    return null;
+  }
+
+  Future<String?> changePassword({required String email}) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        return "E-mail not found";
+      }
+      return e.code;
+    }
+    return null;
+  }
+
+  Future<String?> signOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } on FirebaseAuthException catch (e) {
+      return e.code;
+    }
+    return null;
+  }
+
+  Future<String?> delete({required String password}) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: _firebaseAuth.currentUser!.email!,
+        password: password,
+      );
+      await _firebaseAuth.currentUser!.delete();
+    } on FirebaseAuthException catch (e) {
+      return e.code;
+    }
+    return null;
+  }
+}
